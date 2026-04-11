80aceed2bb5ad3fb1d572d806bd1e97f12dffec0
unknown <pop.naren@gmail.com>
2026-04-11 12:46:56 +0530

    Added new features and fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { FavoritesProvider } from "./context/FavoritesContext";
+import { FavoritesProvider } from "./context/FavoritesContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <FavoritesProvider>
